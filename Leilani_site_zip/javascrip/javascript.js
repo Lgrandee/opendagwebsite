dd66056092f74affe1fc09b69611208d3f36c47d
Lgrandee <148849206+Lgrandee@users.noreply.github.com>
2024-09-09 10:19:31 +0200

    veel werk

--- Leilani_site_zip/javascrip/javascript.js
+++ Leilani_site_zip/javascrip/javascript.js
@@ -16,4 +16,25 @@ tl.staggerFrom("li", 2, {
   opacity: 0, 
   ease: Back.easeOut
 }, 0.2);
+const indicator = document.querySelector('.nav-indicator');
+const items = document.querySelectorAll('.nav-item');
 
+function handleIndicator(el) {
+  items.forEach(item => {
+    item.classList.remove('is-active');
+    item.removeAttribute('style');
+  });
+  
+  indicator.style.width = `${el.offsetWidth}px`;
+  indicator.style.left = `${el.offsetLeft}px`;
+  indicator.style.backgroundColor = el.getAttribute('active-color');
+
+  el.classList.add('is-active');
+  el.style.color = el.getAttribute('active-color');
+}
+
+
+items.forEach((item, index) => {
+  item.addEventListener('click', (e) => { handleIndicator(e.target)});
+  item.classList.contains('is-active') && handleIndicator(item);
+});
